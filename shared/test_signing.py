d9ad53522b944947b9ece868f15c9c61060317b7
Tillmann Heidsieck <theidsieck@leenox.de>
2020-12-13 14:42:56 +0100

    build: add test if signing would be possible before selecting ARDUINO_SIGNING=0|1
    Signed-off-by: Tillmann Heidsieck <theidsieck@leenox.de>

---
+++ shared/test_signing.py
@@ -0,0 +1,33 @@
+#!/bin/python
+#
+# (C) Copyright 2020 Tillmann Heidsieck
+#
+# SPDX-License-Identifier: MIT
+#
+
+from OpenSSL import crypto
+import os
+
+Import("env")
+
+s = 1
+try:
+    pkey = os.environ["FIRMWARE_SIGNING_PKEY"]
+except:
+    s = 0
+
+if s:
+    if os.path.exists(pkey):
+        try:
+            with open(pkey, "r") as f:
+                pkey = f.read()
+        except:
+            s = 0
+if s:
+    try:
+        pk = crypto.load_privatekey(crypto.FILETYPE_PEM, pkey)
+    except:
+        s = 0
+
+signing = "-DARDUINO_SIGNING=%d" % s
+env.Append(CPPFLAGS=[signing])
