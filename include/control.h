52e367a4616a535657b355041baf976efd7608b9
Tillmann Heidsieck <theidsieck@leenox.de>
2020-12-21 16:55:50 +0100

    firmware: extract control logic and move to class
    The control logic for the firmware is extracted and moved to a separate class.
    
    Signed-off-by: Tillmann Heidsieck <theidsieck@leenox.de>

---
+++ include/control.h
@@ -0,0 +1,46 @@
+/*
+ * (C) Copyright 2020 Tillmann Heidsieck
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+#include <Arduino.h>
+
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>
+
+class FirmwareControl {
+private:
+    String wifi_ssid;
+    String wifi_pass;
+
+    String ctrl_url;
+    String update_url;
+
+    BearSSL::CertStore cert_store;
+
+    bool go_online_request;
+    bool online;
+
+    std::unique_ptr<BearSSL::WiFiClientSecure>wcs;
+    HTTPClient https;
+
+    WiFiState *wss;
+
+protected:
+    void OTA();
+    void query_ctrl();
+    void go_online();
+    void set_clock();
+    void deep_sleep();
+
+public:
+    bool is_online() { return this->online; }
+    void online_request() { go_online_request = true; }
+
+    void loop();
+    void setup();
+
+    FirmwareControl();
+    ~FirmwareControl() {};
+};
--- src/main.cpp
+++ src/main.cpp
@@ -6,122 +6,20 @@
  */
 #include <Arduino.h>
 
-#include <ArduinoJson.h>
-#include <CertStoreBearSSL.h>
-#include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
-#include <FS.h>
-#include <LittleFS.h>
-#include <time.h>
+#include "control.h"
 
-#include "updater.h"
 
-BearSSL::CertStore cert_store;
-
-String wifi_ssid;
-String wifi_pass;
-String ctrl_url;
-String update_url;
-
-void setClock() {
-    char buffer[64];
-
-    configTime(0, 0, "pool.ntp.org", "time.nist.gov");
-
-    Serial.print(F("Waiting for NTP time sync: "));
-    time_t now = time(nullptr);
-    while (now < 8 * 3600 * 2) {
-        yield();
-        delay(500);
-        Serial.print(F("."));
-        now = time(nullptr);
-    }
-
-    Serial.println(F(""));
-    struct tm timeinfo;
-    gmtime_r(&now, &timeinfo);
-    Serial.print(F("Current time: "));
-    strftime(buffer, 64, "%c", &timeinfo);
-    Serial.println(buffer);
-}
+FirmwareControl ctrl;
 
 void setup() {
     Serial.begin(115200);
     Serial.println();
     Serial.printf("ESP8266 Firmware Version %s (%s)\n", VERSION, DATE);
-
-    LittleFS.begin();
-
-    int numCerts = cert_store.initCertStore(LittleFS, PSTR("/certs.idx"), PSTR("/certs.ar"));
-    Serial.print(F("Number of CA certs read: "));
-    Serial.println(numCerts);
-    if (numCerts == 0)
-        Serial.println(F("No certs found"));
-
-    File file = LittleFS.open("/config.js", "r");
-
-    StaticJsonDocument<1024> doc;
-    DeserializationError error = deserializeJson(doc, file);
-    if (error != DeserializationError::Ok) {
-        Serial.println(F("Could not load config file"));
-        return;
-    }
-    file.close();
-
-    wifi_ssid = doc["wifi_ssid"] | "NO SSID";
-    wifi_pass = doc["wifi_pass"] | "NO PSK";
-    ctrl_url  = (doc["ctrl_url"] | "https://example.com");
-
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(wifi_ssid, wifi_pass);
+    ctrl.setup();
 }
 
 void loop() {
-    /* work around problems of WiFi status not always showing WL_CONNECTED */
-    if ((WiFi.status() == WL_CONNECTED) || WiFi.localIP().isSet()) {
-        static char c;
-        if (!c)
-            setClock();
-        c = 1;
-
-        {
-            std::unique_ptr<BearSSL::WiFiClientSecure>wcs(new BearSSL::WiFiClientSecure);
-            wcs->setCertStore(&cert_store);
-
-            StaticJsonDocument<1024> doc;
-            String server = ctrl_url;
-            server.remove(0, server.indexOf(":") + 3);
-            server.remove(server.indexOf("/"));
-            bool mfln = wcs->probeMaxFragmentLength(server, 443, 512);
-            if (mfln)
-                wcs->setBufferSizes(512, 512);
-            else
-                wcs->setBufferSizes(16384, 512);
-
-            HTTPClient https;
-            if (https.begin(*wcs, ctrl_url)) {
-                int http_code = https.GET();
-                if (http_code > 0) {
-                    if (http_code == HTTP_CODE_OK) {
-                        String payload = https.getString();
-                        DeserializationError error = deserializeJson(doc, payload.c_str());
-                        if (error != DeserializationError::Ok)
-                            Serial.println(F("Could not load config file"));
-                        update_url = doc["firmware_update"] | "";
-                    }
-                }
-            }
-            https.end();
-
-            if (!update_url.length()) {
-                Updater upd;
-                upd.update(https, update_url);
-            }
-            wcs->stop();
-        }
-
-        Serial.println(F("loop end"));
-    }
-
+    ctrl.loop();
     delay(500);
 }
