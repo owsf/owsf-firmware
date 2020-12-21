86ecf8601ba4832d28cf8e172d2d1d48557e6fe8
Tillmann Heidsieck <theidsieck@leenox.de>
2020-12-21 16:51:27 +0100

    src: updater: fix verification
    Signed-off-by: Tillmann Heidsieck <theidsieck@leenox.de>

--- include/updater.h
+++ include/updater.h
@@ -7,8 +7,15 @@
 #ifndef _UPDATER_H_
 #define _UPDATER_H_
 
+#if SIGNED_UPDATES
+#include <CertStoreBearSSL.h>
+#include <BearSSLHelpers.h>
+#endif
+
 #include <ESP8266HTTPClient.h>
 #include <ESP8266httpUpdate.h>
+
+#include "pubkey.h"
 
 class Updater : public ESP8266HTTPUpdate {
 private:
@@ -21,8 +28,12 @@ private:
 public:
     int update(HTTPClient &, String &);
 
-    Updater() {};
-    ~Updater() {};
+#if SIGNED_UPDATES
+    Updater() : sign_pubkey(pubkey), sign(&sign_pubkey) {}
+#else
+    Updater() {}
+#endif
+    ~Updater() {}
 };
 
 #endif
