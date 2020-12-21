24cfa3ee5bd7c425eb89cd0544152f6f7b679a16
Tillmann Heidsieck <theidsieck@leenox.de>
2020-12-21 07:11:47 +0100

    updater: major rework
    Inherit from ESP8266HTTPUpdate. Add custom update function, circumventing the
    usual update functions and recycle an external HTTPClient object.
    
    Signed-off-by: Tillmann Heidsieck <theidsieck@leenox.de>

---
+++ include/updater.h
@@ -0,0 +1,28 @@
+/*
+ * (C) Copyright 2020 Tillmann Heidsieck
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+#ifndef _UPDATER_H_
+#define _UPDATER_H_
+
+#include <ESP8266HTTPClient.h>
+#include <ESP8266httpUpdate.h>
+
+class Updater : public ESP8266HTTPUpdate {
+private:
+#if SIGNED_UPDATES
+    BearSSL::PublicKey sign_pubkey;
+    BearSSL::HashSHA256 hash;
+    BearSSL::SigningVerifier sign;
+#endif
+
+public:
+    int update(HTTPClient &http, const char *uu);
+
+    Updater() {};
+    ~Updater() {};
+};
+
+#endif
