7789c421230e59f3bce07a6d9116abb09de3ec50
Tillmann Heidsieck <theidsieck@leenox.de>
2020-12-21 07:14:43 +0100

    firmware: add POC for updated over secure http connection
    Signed-off-by: Tillmann Heidsieck <theidsieck@leenox.de>

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,127 @@
  * SPDX-License-Identifier: MIT
  *
  */
-
-
 #include <Arduino.h>
 
-void setup() {
+#include <ArduinoJson.h>
+#include <CertStoreBearSSL.h>
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>
+#include <FS.h>
+#include <LittleFS.h>
+#include <time.h>
+
+#include "updater.h"
+
+BearSSL::CertStore cert_store;
+
+static char *wifi_ssid;
+static char *wifi_pass;
+static char *remote_server;
+
+static char *update_url;
+
+void setClock() {
+    char buffer[64];
+
+    configTime(0, 0, "pool.ntp.org", "time.nist.gov");
+
+    Serial.print(F("Waiting for NTP time sync: "));
+    time_t now = time(nullptr);
+    while (now < 8 * 3600 * 2) {
+        yield();
+        delay(500);
+        Serial.print(F("."));
+        now = time(nullptr);
+    }
+
+    Serial.println(F(""));
+    struct tm timeinfo;
+    gmtime_r(&now, &timeinfo);
+    Serial.print(F("Current time: "));
+    strftime(buffer, 64, "%c", &timeinfo);
+    Serial.println(buffer);
 }
 
+void setup() {
+    Serial.begin(115200);
+    Serial.println();
+    Serial.printf("ESP8266 Firmware Version %s (%s)\n", VERSION, DATE);
+
+    LittleFS.begin();
+
+    int numCerts = cert_store.initCertStore(LittleFS, PSTR("/certs.idx"), PSTR("/certs.ar"));
+    Serial.print(F("Number of CA certs read: "));
+    Serial.println(numCerts);
+    if (numCerts == 0)
+        Serial.println(F("No certs found"));
+
+    File file = LittleFS.open("/config.js", "r");
+
+    StaticJsonDocument<1024> doc;
+    DeserializationError error = deserializeJson(doc, file);
+    if (error != DeserializationError::Ok) {
+        Serial.println(F("Could not load config file"));
+        return;
+    }
+    file.close();
+
+    wifi_ssid = strdup(doc["wifi_ssid"] | "NO SSID");
+    wifi_pass = strdup(doc["wifi_pass"] | "NO PSK");
+    remote_server = strdup(doc["remote_server"] | "example.com");
+
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(wifi_ssid, wifi_pass);
+}
 
 void loop() {
+    static char c;
+    static int i = 0;
+
+    /* work around problems of WiFi status not always showing WL_CONNECTED */
+    if ((WiFi.status() == WL_CONNECTED) || WiFi.localIP().isSet()) {
+        if (!c)
+            setClock();
+        c = 1;
+
+        {
+            std::unique_ptr<BearSSL::WiFiClientSecure>wcs(new BearSSL::WiFiClientSecure);
+            wcs->setCertStore(&cert_store);
+
+            StaticJsonDocument<1024> doc;
+            bool mfln = wcs->probeMaxFragmentLength(remote_server, 443, 512);
+            if (mfln)
+                wcs->setBufferSizes(512, 512);
+            else
+                wcs->setBufferSizes(16384, 512);
+
+            HTTPClient https;
+            if (https.begin(*wcs, remote_server, 443, "/esp8266.json", true)) {
+                int http_code = https.GET();
+                if (http_code > 0) {
+                    if (http_code == HTTP_CODE_OK) {
+                        String payload = https.getString();
+                        DeserializationError error = deserializeJson(doc, payload.c_str());
+                        if (error != DeserializationError::Ok) {
+                            Serial.println(F("Could not load config file"));
+                        }
+                        if (!doc["firmware_update"].isNull()) {
+                            update_url = strdup(doc["firmware_update"]);
+                        }
+                    }
+                }
+            }
+            https.end();
+
+            if (update_url) {
+                Updater upd;
+                upd.update(https, update_url);
+            }
+            wcs->stop();
+        }
+
+        Serial.println(F("loop end"));
+    }
+
+    delay(500);
 }
