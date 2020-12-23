dc57a1c60ad0a63c0ad27b3b274139621e503ad5
Tillmann Heidsieck <theidsieck@leenox.de>
2020-12-23 21:03:30 +0100

    misc: create device specific config. The device specific config includes the connected sensors as well as the device name and the sleep time
    Signed-off-by: Tillmann Heidsieck <theidsieck@leenox.de>

--- misc/config.tmpl.js
+++ misc/config.tmpl.js
@@ -1,6 +1,18 @@
 {
-    "wifi_ssid" : "@WIFI_SSID@",
-    "wifi_pass" : "@WIFI_PASS@",
-    "ctrl_url"  : "@CTRL_SERVER_URL@",
-    "sleep_time_s": "60"
+    "device_name" : "test sensor",
+    "sleep_time_s" : 60,
+    "sensors" : [
+        {
+            "type" : "ADC",
+            "R1"   : 47000.0,
+            "R2"   : 9100.0,
+            "offset" : 0.0,
+            "factor": 1.0
+        },
+        {
+            "type" : "BME280",
+            "scl"  : 14,
+            "sda"  : 2
+        }
+    ]
 }
