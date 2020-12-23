55363407dacff3f75e990fc0fac79dea914a2c98
Tillmann Heidsieck <theidsieck@leenox.de>
2020-12-23 21:02:47 +0100

    misc: create template for global config. The global config includes influxDB settings as well as wifi credentials
    Signed-off-by: Tillmann Heidsieck <theidsieck@leenox.de>

---
+++ misc/global_config.tmpl.js
@@ -0,0 +1,9 @@
+{
+    "wifi_ssid"  : "@WIFI_SSID@",
+    "wifi_pass"  : "@WIFI_PASS@",
+    "ctrl_url"   : "@CTRL_SERVER_URL@",
+    "influx_url" : "@INFLUX_URL@",
+    "influx_token"  : "@INFLUX_TOKEN@",
+    "influx_org": "@INFLUX_ORG@",
+    "influx_bucket": "@INFLUX_BUCKET@"
+}
