f0b6637a6bb9ece48109e8577395d8693a1a4c8a
Dominik Laton <dominik.laton@web.de>
2021-02-13 22:42:05 +0100

    rtcmem_map: added json to address function
    Signed-off-by: Dominik Laton <dominik.laton@web.de>

--- include/rtcmem_map.h
+++ include/rtcmem_map.h
@@ -6,6 +6,7 @@
  */
 #ifndef _RTCMEM_MAP_H_
 #define _RTCMEM_MAP_H_
+#include <ArduinoJson.h>
 
 #define RTCMEM_WSS RTC_USER_MEM
 #define RTCMEM_SENSOR0 (128 - 32)
@@ -14,5 +15,37 @@
 #define RTCMEM_SENSOR2 (RTCMEM_SENSOR1 + 8)
 #define RTCMEM_SENSOR3 (RTCMEM_SENSOR2 + 8)
 #define RTCMEM_SENSOR4 (RTCMEM_SENSOR3 + 8)
+
+inline int get_rtc_addr(const JsonVariant &j)
+{
+	int addr;
+	int mem_no;
+
+	addr = -1;
+	mem_no = j["RTCMEM_SENSOR"] | -1;
+
+	switch(mem_no)
+	{
+	case 0:
+		addr = RTCMEM_SENSOR0;
+		break;
+	case 1:
+		addr = RTCMEM_SENSOR1;
+		break;
+	case 2:
+		addr = RTCMEM_SENSOR2;
+		break;
+	case 3:
+		addr = RTCMEM_SENSOR3;
+		break;
+	case 4:
+		addr = RTCMEM_SENSOR4;
+		break;
+	default:
+		break;
+	}
+
+	return addr;
+}
 
 #endif
