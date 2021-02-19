6fb82489265fdc646cf09f8d5276491e1f455d95
Tillmann Heidsieck <theidsieck@leenox.de>
2021-02-19 21:27:14 +0100

    rtcmem: remove magic numbers
    Signed-off-by: Tillmann Heidsieck <theidsieck@leenox.de>

--- include/rtcmem_map.h
+++ include/rtcmem_map.h
@@ -8,7 +8,14 @@
 #define _RTCMEM_MAP_H_
 
 #define RTCMEM_WSS RTC_USER_MEM
-#define RTCMEM_SENSOR_BASE (128 - 40)
-#define RTCMEM_SENSOR(i) (i < 0 || i > 4 ? -1 : (RTCMEM_SENSOR_BASE + 8 * (i)))
+#define RTCMEM_SENSOR_SLOT_SIZE 8
+#define RTCMEM_SENSOR_SLOT_COUNT 5
+#define RTCMEM_SENSOR_BASE \
+    (128 - RTCMEM_SENSOR_SLOT_COUNT * RTCMEM_SENSOR_SLOT_SIZE)
+#define RTCMEM_SENSOR(i) \
+    (\
+     (i) < 0 || (i) >= RTCMEM_SENSOR_SLOT_COUNT ? \
+     -1 : (RTCMEM_SENSOR_BASE + RTCMEM_SENSOR_SLOT_SIZE * (i))\
+    )
 
 #endif
