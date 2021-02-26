4a140c9e408112dfd811e0a3df9d535167f6d260
Dominik Laton <dominik.laton@web.de>
2021-02-26 17:24:32 +0100

    shared: custom_targets: beautified
    Signed-off-by: Dominik Laton <dominik.laton@web.de>

--- shared/custom_targets.py
+++ shared/custom_targets.py
@@ -2,14 +2,14 @@ Import("env")
 
 if env['PIOENV'] == "release":
     env.AddCustomTarget(
-            "generate_control",
-            "$BUILD_DIR/${PROGNAME}.bin",
-            "misc/generate_control.py"
+        "generate_control",
+        "$BUILD_DIR/${PROGNAME}.bin",
+        "misc/generate_control.py"
     )
 else:
     env.AddCustomTarget(
-            "generate_control",
-            "$BUILD_DIR/${PROGNAME}.bin",
-            "true"
+        "generate_control",
+        "$BUILD_DIR/${PROGNAME}.bin",
+        "true"
     )
 
