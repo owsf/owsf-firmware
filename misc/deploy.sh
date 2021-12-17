87fbf61056ab8414d46cee910a6774d12d7a200e
Tillmann Heidsieck <theidsieck@leenox.de>
2021-12-17 11:31:56 +0100

    fix deploy script
    Signed-off-by: Tillmann Heidsieck <theidsieck@leenox.de>

--- misc/deploy.sh
+++ misc/deploy.sh
@@ -9,6 +9,8 @@ curl -v -H 'Content-Type: application/json' -H "X-auth-token: $IOTA_TOKEN" \
     -H "X-global-config-key: $(cat data/global_config.json | jq -M -r .global_config_key)" \
     -X PUT -d "$(cat data/global_config.json)" $IOTA_URL/api/v1/deploy/global_config
 
-curl -v -H "Content-Type: text/plain" -H "X-auth-token: $IOTA_TOKEN" \
-    -H "X-firmware-version: $(cat server_data/firmware.json | jq -M -r .version)" \
-    -X PUT -d "@server_data/$(cat server_data/firmware.json | jq -M -r .file)" $IOTA_URL/api/v1/deploy/firmware
+FW_VERSION=$(cat server_data/firmware.json | jq -M -r .version)
+FW_FILE="server_data/$(cat server_data/firmware.json | jq -M -r .file)"
+base64 $FW_FILE | curl -v -H "Content-Type: text/plain" -H "X-auth-token: $IOTA_TOKEN" \
+    -H "X-firmware-version: $FW_VERSION" \
+    -X PUT --data-binary "@-" $IOTA_URL/api/v1/deploy/firmware
